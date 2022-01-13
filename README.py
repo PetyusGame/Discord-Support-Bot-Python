3e57dc39aa4d907cec85dc7e5fbaa143991489ef
PetyusGame <89593853+PetyusGame@users.noreply.github.com>
2022-01-13 16:10:21 +0100

    Add files via upload

---
+++ README.py
@@ -0,0 +1,11 @@
+# Discord Support Bot Python
+
+> This is a simple asset of a Discord Support Bot! You can copy the code, and rewrite the assets!
+*note that some code lines may note be correct! You can remove those or just rewrite them*
+
+---
+
+### Information
+
+Code by:
+	- [f1oppa] (#github.com/f1oppa)
---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+import discord
+import time
+from discord.ext import commands
+"""from keep_alive import keep_alive"""
+
+client = discord.Client()
+
+bot = commands.Bot(command_prefix="your_bot_prefix")
+bot.remove_command("help")
+
+# made by f1oppa (github.com/f1oppa)
+
+
+@bot.command(name="your_command") # <------ this command will make the bot create a channel
+async def getsupport(ctx: commands.Context):
+    overwrites = {
+        ctx.guild.default_role: discord.PermissionOverwrite(view_channel=False),
+        ctx.author: discord.PermissionOverwrite(view_channel=True)
+    }
+    channel = await ctx.guild.create_text_channel(f'support-room', overwrites=overwrites, category=discord.utils.get(ctx.guild.categories, name='support')) # <------- 'support-room' is you channel name | 'support is your category name' (you can rename them!)
+    await ctx.send(f"Your support channel: <#{channel.id}>") # <------- this is what the bot will respond when the channel is created
+    await channel.send(
+        f"this is the text the bot will reply in that privately created channel | <@{ctx.author.id}> use this to mention the member!"
+    )
+
+
+@bot.command(name="channel.close") <------- this command will delete the channel
+async def closechannel(ctx: commands.Context):
+    if ctx.channel.name.startswith("support-"):
+        await ctx.send("Ok. The channel will be deleted in 10 seconds.") # <---- you can rewrite this as well
+        time.sleep(10)
+        await ctx.channel.delete()
+    else:
+        await ctx.send("This is not a support channel.") # <------ this is an error message if you close the channel in another channel
+
+
+my_secret = os.environ['token']
+"""keep_alive()"""
+bot.run(my_secret)
